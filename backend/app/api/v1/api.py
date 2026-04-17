d92753e85a3b61aeb9c0fc3f4e53e1dbb8f1a0bf
babcoh2 <henry.babcock@outlook.com>
2026-04-16 22:06:35 -0400

    added player sync

--- backend/app/api/v1/api.py
+++ backend/app/api/v1/api.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter
-
-api_router = APIRouter()
-
+from app.api.v1.endpoints import sportmonks # Import your new file
 from app.api.v1.endpoints import leagues
 
 api_router = APIRouter()
 
+api_router.include_router(sportmonks.router, prefix="/sportmonks", tags=["sportmonks"])
 api_router.include_router(leagues.router, prefix="/leagues", tags=["leagues"])
